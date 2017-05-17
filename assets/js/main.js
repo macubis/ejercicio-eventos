c5f5d20d8109d28b2d3fada67ee1729ece289b37
macubis <macubis@gmail.com>
2017-05-17 00:47:59 -0400

    subiendo ejercicio de eventos

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+document.getElementById("demo").addEventListener("click", function hacerClick(){
+		alert("¡HOLA MUNDO!");
+})
+
+function miFuncion(){
+	alert("PAGINA CARGADA");
+}
+miFuncion();
+function sobre(){
+	alert("ESTAS SOBRE MI");
+}
+sobre();
+function fuera(){
+	alert("ESTAS FUERA");
+
+};
+fuera();
+
